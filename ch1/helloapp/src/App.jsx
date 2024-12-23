32567668690519fcb7c39a8366b735191dfd563c
Tyler Degen <tddegen@gmail.com>
2024-12-23 13:57:00 -0500

    counter func before lifting state

--- ch1/helloapp/src/App.jsx
+++ ch1/helloapp/src/App.jsx
@@ -1,31 +1,21 @@
-const Hello = ({name,age}) => {
-  const bornYear = () => new Date().getFullYear() - age
+import { useState } from 'react'
 
+const App = () => {
+  const [ counter, setCounter ] = useState(0)
+
+  const incremenentCounterByOne = () => {
+    setCounter(counter + 1)
+  }
+
+  console.log("rendering")
   return (
-    <div>
-      <p>
-        Hello {name}, you are {age} years old and were born in {bornYear()}
-      </p>
-    </div>
+    <>    
+      <div>{counter}</div>
+      <button onClick={incremenentCounterByOne}>plus</button>
+      <button onClick={() => setCounter(0)}>zero</button>
+    </>
+
   )
-}
-
-const App = (props) => {
-  const {counter} = props
-  return (
-    <div>{counter}</div>
-  )
-
-  // const name = 'Peter'
-  // const age = 10
-
-  // return (
-  //   <div>
-  //     <h1>Greetings</h1>
-  //     <Hello name="Maya" age={26 + 10} />
-  //     <Hello name={name} age={age} />
-  //   </div>
-  // )
 }
 
 export default App
--- ch1/helloapp/src/main.jsx
+++ ch1/helloapp/src/main.jsx
@@ -2,17 +2,4 @@ import ReactDOM from 'react-dom/client'
 
 import App from './App'
 
-let counter = 1
-
-const root = ReactDOM.createRoot(document.getElementById('root'))
-const refresh = () => {
-  root.render(
-    <App counter={counter} />
-  )
-}
-
-refresh()
-counter += 1
-refresh()
-counter +=1
-refresh()
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
