e544538c01409ca743d39728b8e9bb042910d493
Tyler Degen <tddegen@gmail.com>
2024-12-23 14:04:52 -0500

    some stateful stuff, gonna make more efficient

--- ch1/helloapp/src/App.jsx
+++ ch1/helloapp/src/App.jsx
@@ -1,18 +1,35 @@
 import { useState } from 'react'
 
+const Display = ({counter}) => {
+  return (
+    <div>{counter}</div>
+  )
+}
+
+const Button = ({onClick, text}) => { 
+  return (
+    <button onClick = {onClick}>
+      {text}
+    </button>
+  )
+}
+
 const App = () => {
   const [ counter, setCounter ] = useState(0)
 
   const incremenentCounterByOne = () => {
     setCounter(counter + 1)
   }
+  const decreaseCounterByOne = () => setCounter(counter-1)
 
   console.log("rendering")
   return (
     <>    
-      <div>{counter}</div>
-      <button onClick={incremenentCounterByOne}>plus</button>
-      <button onClick={() => setCounter(0)}>zero</button>
+      <Display counter={counter}></Display>
+      <Button onClick={incremenentCounterByOne} text="plus"></Button>
+      <Button onClick={decreaseCounterByOne} text="minus"></Button>
+
+      <Button onClick={() => setCounter(0)} text="zero"></Button>
     </>
 
   )
