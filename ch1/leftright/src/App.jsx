0154dea09946aa02ecd29b503f716b8859832348
Tyler Degen <tddegen@gmail.com>
2024-12-23 21:32:59 -0500

    weird closure function stuff that spits out a function in the attribute

--- ch1/leftright/src/App.jsx
+++ ch1/leftright/src/App.jsx
@@ -1,10 +1,52 @@
 import { useState } from 'react'
 
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing buttons
+      </div>
+    )
+  }
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
+const Button = ({handleClick, text}) => {
+  return(  
+  <button onClick={handleClick}>
+    {text}
+  </button>)
+}
+
 const App = () => {
   const [left, setLeft] = useState(0)
   const [right, setRight] = useState(0)
   const [allClicks, setAll] = useState([])
   const [total, setTotal] = useState(0)
+  const [value, setValue] = useState(0)
+
+  const handleClick = () => {
+    console.log("clicked")
+  }
+
+  const hello = (who) => {
+    const handler = () => {
+      console.log('hello', who)
+    }
+    return handler
+  }
+
+  const denseHello = (who) => {
+    return () => {
+      console.log("hello", who)
+    }
+  }
+
+  const setToValue = (newValue) => () => setValue(newValue)
 
   const handleLeftClick = () => { 
     setAll(allClicks.concat('L')); 
@@ -21,15 +63,14 @@ const App = () => {
 
   return (
     <div>
+      <button onClick={hello('world')}>Closure stuff</button>
       {left}
-      <button onClick={handleLeftClick}>
-        left
-      </button>
-      <button onClick={handleRightClick}>
-        right
-      </button>
+      <Button handleClick={handleClick} text="handleClick"/>
+      <Button handleClick={handleLeftClick} text='LLeft' />
+      <Button handleClick={handleRightClick} text='RRight' />
       {right}
-      <p>{total} All clicks {allClicks.join(",")}</p>
+      <History allClicks={allClicks} />
+      <button onClick={setToValue(1000)}>thousand</button>
     </div>
   )
 }
