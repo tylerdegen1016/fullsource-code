88f8dd743d4779b5ca60f03519e569ecf7ff3d86
Tyler Degen <tddegen@gmail.com>
2024-12-27 15:54:51 -0500

    broke course out into component

--- ch2/courseinfo/src/App.jsx
+++ ch2/courseinfo/src/App.jsx
@@ -1,12 +1,4 @@
-const Course = ({course, parts}) => {
-  return(
-    <div>
-    <Header course={course} />
-    <Content parts={parts}/>
-    <Total parts={parts}/>
-  </div>
-  )
-}
+import Course from "./components/Course"
 
 const App = () => {
   const course = 'Half Stack application development'
@@ -31,35 +23,6 @@ const App = () => {
   )
 }
 
-const Header = (props) => {
-  return(
-    <h1>{props.course}</h1>
-  )
-}
 
-const Content = (props) => {
-  return(
-    <>
-      <Part part={props.parts[0]}/>
-      <Part part={props.parts[1]}/>
-      <Part part={props.parts[2]}/>
-    </>
-  )
-
-}
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.part.name} {props.part.exercises}
-    </p>
-  )
-}
-
-const Total = (props) => {
-  return(
-    <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
-  )
-}
 
 export default App
