69478a4c9fe86816cd1b7fbe3b7044cb577f8818
Tyler Degen <tddegen@gmail.com>
2024-12-28 16:53:00 -0500

    playing with hooks

--- ch2/notes/src/App.jsx
+++ ch2/notes/src/App.jsx
@@ -1,11 +1,37 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
+import axios from 'axios'
 import Note from './components/Note'
 //ch2/notes/components/Note.js
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
+const App = () => {
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState("a new note")
   const [showAll, setShowAll] = useState(true)
+
+  // const hook = () => {
+  //   console.log("effect")
+  //   axios
+  //     .get('http://localhost:3001/notes')
+  //     .then(response => {
+  //       console.log('promise fulfilled')
+  //       setNotes(response.data)
+  //     })
+  // }
+  // }
+
+  // useEffect(hook, [])
+  useEffect(() => {
+    console.log("effect")
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log("promise fulfilled")
+        setNotes(response.data)
+      })
+  }, [])
+
+  console.log("render", notes.length, "notes")
+  
 
   const notesToShow = showAll ? notes : notes.filter(note => note.important)
 
--- ch2/notes/src/main.jsx
+++ ch2/notes/src/main.jsx
@@ -1,42 +1,4 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-import axios from 'axios'
-
-import App from './App'
-
-const promise = axios.get('http://localhost:3001/notes')
-console.log(promise)
-promise.then(response => console.log(response))
-
-axios
-  .get('http://localhost:3001/notes')
-  .then(response => {
-    const notes = response.data
-    console.log(notes)
-})
-
-const promise2 = axios.get('http://localhost:3001/foobar')
-console.log(promise2)
-
-const notes = [
-  {
-    id: 1,
-    content: 'HTML is easy',
-    important: true
-  },
-  {
-    id: 2,
-    content: 'Browser can execute only JavaScript',
-    important: false
-  },
-  {
-    id: 3,
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    important: true
-  }
-]
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <App notes={notes} />
-)
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
