8ba80f5ee0c33eb7165fb1072915c813f2a8f3e0
Tyler Degen <tddegen@gmail.com>
2024-12-28 00:17:38 -0500

    db.json install, playing with axios calls

--- ch2/notes/src/main.jsx
+++ ch2/notes/src/main.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+import axios from 'axios'
+
 import App from './App'
+
+const promise = axios.get('http://localhost:3001/notes')
+console.log(promise)
+promise.then(response => console.log(response))
+
+axios
+  .get('http://localhost:3001/notes')
+  .then(response => {
+    const notes = response.data
+    console.log(notes)
+})
+
+const promise2 = axios.get('http://localhost:3001/foobar')
+console.log(promise2)
 
 const notes = [
   {
