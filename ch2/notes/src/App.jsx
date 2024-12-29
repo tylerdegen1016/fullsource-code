ab2c15369a42c9bb0f17de3843b0d67f19207815
Tyler Degen <tddegen@gmail.com>
2024-12-29 18:12:29 -0500

    updated st service now returns data instead of promise

--- ch2/notes/src/App.jsx
+++ ch2/notes/src/App.jsx
@@ -15,8 +15,8 @@ const App = () => {
 
     noteService
       .update(id, changedNote)
-      .then(response => {
-        setNotes(notes.map(note => note.id === id ? response.data: note))
+      .then(returnedNote => {
+        setNotes(notes.map(note => note.id === id ? returnedNote : note))
       })
   }
 
@@ -35,8 +35,8 @@ const App = () => {
   useEffect(() => {
     noteService
       .getAll()
-      .then(response => {
-      setNotes(response.data)
+      .then(initialNotes => {
+      setNotes(initialNotes)
     })
   }, [])
 
@@ -60,8 +60,8 @@ const App = () => {
 
     noteService
       .create(noteObject)
-      .then(response => {
-        setNotes(notes.concat(response.data))
+      .then(returnedNote => {
+        setNotes(notes.concat(returnedNote))
         setNewNote('')
       })
   }
