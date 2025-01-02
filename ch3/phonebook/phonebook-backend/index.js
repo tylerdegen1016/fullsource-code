f2ccd8683fecda311199912c11e6e2fc10f0e2a7
Tyler Degen <tddegen@gmail.com>
2025-01-02 15:17:55 -0500

    Set up phonebook to deploy to fly.io (relative api url, fly port update, docker ignore remove dist, static content epress and update port in backend, proxy ion vite for testing locally, consolidated scripts in npm

--- ch3/phonebook/phonebook-backend/index.js
+++ ch3/phonebook/phonebook-backend/index.js
@@ -9,6 +9,7 @@ morgan.token('body', req => {
 })
 
 app.use(express.json())
+app.use(express.static('dist'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(cors())
 
@@ -34,6 +35,7 @@ let people = [
       "number": "39-23-6423122"
     }
 ]
+
 
 app.get("/api/people", (req, res) => {
     res.json(people)
@@ -110,64 +112,9 @@ const generateId = () => {
 
 
 
-let notes = [
-  { id: "1", content: "HTML is easy", important: true },
-  { id: "2", content: "Browser can execute only JavaScript", important: false },
-  {
-    id: "3",
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true,
-  },
-];
 
 
-
-app.get("/api/notes", (req, res) => {
-    res.json(notes)
-})
-
-app.get("/api/notes/:id", (req, res) => {
-    const id = req.params.id
-    const note = notes.find(note => note.id === id)
-    
-    if (note) {
-      res.json(note);
-    } else {
-      res.status(404).end();
-    }
-})
-
-app.delete("/api/notes/:id", (req, res) => {
-    const id = req.params.id
-    notes = notes.filter(note => note.id !== id)
-
-    res.status(204).end()
-})
-
-
-  
-  app.post('/api/notes', (request, response) => {
-    const body = request.body
-  
-    if (!body.content) {
-      return response.status(400).json({ 
-        error: 'content missing' 
-      })
-    }
-  
-    const note = {
-      content: body.content,
-      important: Boolean(body.important) || false,
-      id: generateId(),
-    }
-  
-    notes = notes.concat(note)
-  
-    response.json(note)
-  })
-
-
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-    console.log("port")
+    console.log("port " + PORT)
 })
