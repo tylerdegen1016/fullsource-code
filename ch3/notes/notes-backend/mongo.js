37ecda2fb64a44dc354a9c0ce6368b6bc1708745
Tyler Degen <tddegen@gmail.com>
2025-01-04 00:05:08 -0500

    pulling from omngo

--- ch3/notes/notes-backend/mongo.js
+++ ch3/notes/notes-backend/mongo.js
@@ -22,12 +22,19 @@ const noteSchema = new mongoose.Schema({
 
 const Note = mongoose.model("Note", noteSchema)
 
-const note = new Note({
-    content: "HTML is easy",
-    important: true
-})
+// const note = new Note({
+//     content: "HTML is easy",
+//     important: true
+// })
 
-note.save().then(result => {
-    console.log("note saved!")
+// note.save().then(result => {
+//     console.log("note saved!")
+//     mongoose.connection.close()
+// })
+
+Note.find({important: true}).then(result => {
+    result.forEach(note => {
+      console.log(note)
+    })
     mongoose.connection.close()
-})
+  })
