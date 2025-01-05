83ff378c79050b4bdd3556376676d11838010f6f
Tyler Degen <tddegen@gmail.com>
2025-01-05 18:51:11 -0500

    THE GIRLS ARE TALKIN!!

--- my_flask_react_fun/frontend/src/App.js
+++ my_flask_react_fun/frontend/src/App.js
@@ -1,7 +1,16 @@
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [currentTime, setCurrentTime] = useState(0)
+
+  useEffect(() => {
+    fetch('http://localhost:5000/time').then(res => res.json()).then(data => {
+      setCurrentTime(data.time);
+    })
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +26,8 @@ function App() {
         >
           Learn React
         </a>
+
+        <p>CURRENT TIME {currentTime}</p>
       </header>
     </div>
   );
