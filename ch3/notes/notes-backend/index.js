c6f179a888003287163ab278bec98d77f94217c3
Tyler Degen <tddegen@gmail.com>
2025-01-09 10:59:05 -0500

    Trying shit in some other folders, going back to chapter 3

--- ch3/notes/notes-backend/index.js
+++ ch3/notes/notes-backend/index.js
@@ -47,15 +47,15 @@ app.use(requestLogger)
 
 
 
-let notes = [
-  { id: "1", content: "HTML is easy", important: true },
-  { id: "2", content: "Browser can execute only JavaScript", important: false },
-  {
-    id: "3",
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true,
-  },
-];
+// let notes = [
+//   { id: "1", content: "HTML is easy", important: true },
+//   { id: "2", content: "Browser can execute only JavaScript", important: false },
+//   {
+//     id: "3",
+//     content: "GET and POST are the most important methods of HTTP protocol",
+//     important: true,
+//   },
+// ];
 
 app.get('/', (request, response) => {
     response.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
