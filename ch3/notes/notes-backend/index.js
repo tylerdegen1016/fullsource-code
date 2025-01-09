9e0bd1636459cfd9c7fca9a15caef75d6202ee14
Tyler Degen <tddegen@gmail.com>
2025-01-09 11:08:19 -0500

    Updating post and get specific note to work with new mongodb

--- ch3/notes/notes-backend/index.js
+++ ch3/notes/notes-backend/index.js
@@ -70,44 +70,36 @@ app.get('/api/notes', (request, response) => {
 })
 
 app.get("/api/notes/:id", (request, response) => {
-    const id = request.params.id
-    const note = notes.find(note => note.id === id)
-
-    if (note) {
+    Note.findById(request.params.id).then(note => {
         response.json(note)
-    } else {
-        response.status(404).end()
-    }
+    })
 })
 
-const generateId = () => {
-    const maxId = notes.length > 0 ? Math.max(...notes.map(n => Number(n.id))) : 0
-    return String(maxId + 1)
-}
-
 app.post("/api/notes", (request, response) => {
-    if (!body.content) {
-        return response.status(400).json({
-            error:"content missing"
-        })
+    const body = request.body
+
+    if (body.content === undefined) {
+        return response.status(400).json({ error: 'content missing' })
     }
 
-    const note = {
+    const note = new Note({
         content: body.content,
-        important: Boolean(body.imporant) || false,
-        id: generateId()
-    }
+        important: body.important || false
+    })
 
-    notes = notes.concat(note)
-
-    response.json(note)
+    note.save().then(savedNote => {
+        //implicit return? or what's happening here
+        response.json(savedNote)
+    })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
-    const id = request.params.id
-    notes = notes.filter(note => note.id !== id)
+    // const id = request.params.id
+    // notes = notes.filter(note => note.id !== id)
 
-    response.status(204).end()
+    // response.status(204).end()
+    //TODO
+    response.status(400).end()
 })
 
   //after routes so catch requests to nonexistent routes
